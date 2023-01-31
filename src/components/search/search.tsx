9aa73260c46a16c842bcab3dfa46b51aad6e479c
RuneKodehode <rune.kodehode@gmail.com>
2023-01-31 09:17:49 +0100

    added folder and an input box

---
+++ src/components/search/search.tsx
@@ -0,0 +1,11 @@
+export function Search() {
+  return (
+    <div class="border-solid border-2 border-grey p-2">
+      <input
+        class="p-4 border-solid border-2 border-grey"
+        type="string"
+        placeholder="search-bar"
+      />
+    </div>
+  );
+}
